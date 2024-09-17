467d46d909bee8b9fa76ea580482031980e38621
Ian-Hoole <hoolei@sonoma.edu>
2024-09-17 12:41:03 -0700

    added put and fixed logger

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-const {fetchTest} = require('./dao.js');
+const {getTest, getItem, createItem} = require('./dao.js');
+const {logger} = require('./logger.js');
 
-fetchTest();
+const http = require('http');
+
+const testItem = {grocery: "fish", price: 2.4, purchased: false, count: 2};
--- dao.js
+++ dao.js
@@ -1,26 +1,68 @@
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
-const { DynamoDBDocumentClient, GetCommand } = require("@aws-sdk/lib-dynamodb");
+const { DynamoDBDocumentClient, 
+        GetCommand, 
+        PutCommand, 
+        UpdateCommand, 
+        DeleteCommand
+    } = require("@aws-sdk/lib-dynamodb");
+const { v4: uuidv4 } = require('uuid');
+const { logger } = require('./logger.js');
 
 const client = new DynamoDBClient({ region: "us-west-1" }); // replace with your region
 
 const documentClient = DynamoDBDocumentClient.from(client);
 
-const testUuid = "e415d5c9-e946-4061-bcd9-c7fbf0e57e6d";
+const TableName = "grocerytracker";
+// Get functions
+async function getTest() {
+    const testUuid = "e415d5c9-e946-4061-bcd9-c7fbf0e57e6d";
 
-const getCommand = new GetCommand({
-    TableName: "grocerytracker",
-    Key: { 'uuid': testUuid}
-});
+    const getCommand = new GetCommand({
+        TableName,
+        Key: { 'uuid': testUuid }
+    });
 
-async function fetchTest() {
     try{
-        const data = await documentClient.send(getCommand);
-        console.log(data);
+        const data = await documentClient.send(getCommand).then();
+        logger.info(JSON.stringify(data, null, 2));
     }catch(err){
-        console.error(err);
+        logger.error(err);
     }
 }
 
+async function getItem(key) {
+    const getCommand = new GetCommand({
+        TableName,
+        Key: { 'uuid': key }
+    });
+    try {
+        const data = await documentClient.send(getCommand);
+        logger.info(JSON.stringify(data, null, 2));
+        return data
+    } catch (err) {
+        logger.error(err);
+    }
+}
+
+async function createItem(item){
+    item.uuid = uuidv4();
+    const putCommand = new PutCommand({
+        TableName,
+        Item: item
+    });
+
+    try{
+        const data = await documentClient.send(putCommand);
+        logger.info(JSON.stringify(data, null, 2));
+        return item.uuid;
+    } catch (err) {
+        logger.error(err);
+    }
+}
+
+
 module.exports = {
-    fetchTest
+    getTest,
+    getItem,
+    createItem
 }
